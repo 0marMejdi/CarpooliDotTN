57881907aa98fae3c8f6ceab95ffb06c4ae9f3f6
0marMejdi <124160661+0marMejdi@users.noreply.github.com>
2024-01-14 01:40:24 +0100

    Initialized Database connection and packages

---
+++ Models/CarpooliDbContext.cs
@@ -0,0 +1,64 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarpooliDotTN.Models;
+
+public class CarpooliDbContext : DbContext
+{
+    public CarpooliDbContext(DbContextOptions options) :base(options){ }
+    
+    
+    
+    
+    
+    
+    /**
+     * this function is made for those who run this app first time in their local repository
+     * it's to check if there a database under name "CarpooliDotTn" or no
+     * if it does not exist it creates a one before connecting to it without any errors and exceptions
+     * otherwise it does nothing. keep it in case someone tries to download and try it himself
+     */
+    public static void InitializeDatabase()
+    {
+        void CreateDatabase(SqlConnection connection, string databaseName)
+        {
+            using (SqlCommand command = new SqlCommand($"CREATE DATABASE {databaseName}", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        bool DatabaseExists(SqlConnection connection, string databaseName)
+        {
+            using (SqlCommand command = new SqlCommand($"IF DB_ID('{databaseName}') IS NOT NULL SELECT 1 ELSE SELECT 0", connection))
+            {
+                return (int)command.ExecuteScalar() == 1;
+            }
+        }
+        const string connectionStringMaster = "Data Source=(localdb)\\mssqllocaldb; Integrated Security=True";
+        const string databaseName = "CarpooliDotTN";
+        try
+        {
+            
+            using (SqlConnection connection = new SqlConnection(connectionStringMaster))
+            {
+                
+                connection.Open();
+                // Check if the database exists
+                if (DatabaseExists(connection, databaseName))
+                {
+                    Console.WriteLine("Database check completed, no actions needed.");
+                }
+                else
+                {
+                    // Create the database
+                    CreateDatabase(connection, databaseName);
+                    Console.WriteLine("Database not found, new one has been created.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using CarpooliDotTN.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Check if database is fully created or no 
+CarpooliDbContext.InitializeDatabase();
+// Dependency injection 
+builder.Services.AddDbContext<CarpooliDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
