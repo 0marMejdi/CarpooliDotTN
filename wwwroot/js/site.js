843bbb2ef66a0c13ff0492aa2064943fdb479ed1
0marMejdi <124160661+0marMejdi@users.noreply.github.com>
2024-01-19 11:10:48 +0100

    Close/Open Offer Button Click Effects
    • now flash message will pop up that has been added to site.js. whenever you click at the offer button
    • a loading spinner is added: appears when button is clicked until receiving the resonse.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+function popFlash(message,color){
+    let flashAlert = makeFlash(message,color);
+    document.body.appendChild(flashAlert);
+    setTimeout(()=>{
+        if (flashAlert !== null){
+            console.log(flashAlert);
+            //    flashAlert.style.opacity="0";
+            flashAlert.classList.add("fade")
+            flashAlert.style.opacity="0";
+            flashAlert.style.cssText=" opacity: 0 ; ";
+            flashAlert.remove();
+        }
+    },2000);
+}
+function popFlashError(message){
+    popFlash(message,'danger');
+}
+
+function popFlashSuccess(message){
+    popFlash(message,'success');
+}
+function popFlashInfo(message){
+    popFlash(message,'info');
+}
+function makeFlash(message,color){
+    let flashMessage = document.createElement('div');
+    flashMessage.id="flash-alert";
+    flashMessage.classList.add('alert',`alert-${color}`);
+    flashMessage.textContent =message;
+    return flashMessage;
+}
